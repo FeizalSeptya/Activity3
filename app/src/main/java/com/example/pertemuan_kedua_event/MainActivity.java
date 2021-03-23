a075787b9632d86bb5e65995e3959bec692074e6
FeizalSeptya <izal1309@gmail.com>
2021-03-23 18:46:52 +0700

    Activity Menu dan Floating Button

--- app/src/main/java/com/example/pertemuan_kedua_event/MainActivity.java
+++ app/src/main/java/com/example/pertemuan_kedua_event/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.pertemuan_kedua_event;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -82,21 +85,37 @@ public class MainActivity extends AppCompatActivity {
 
                     //berpindah ke ActivityHasil
                     startActivity(i);
-                }
-                else if (!Email.equals("admin@mail.com") && Password.equals("123")) {
+                } else if (!Email.equals("admin@mail.com") && Password.equals("123")) {
                     Toast.makeText(getApplicationContext(),
                             "Email Salah", Toast.LENGTH_LONG).show();
-                }
-                else if (Email.equals("admin@mail.com") && !Password.equals("123")) {
+                } else if (Email.equals("admin@mail.com") && !Password.equals("123")) {
                     Toast.makeText(getApplicationContext(),
                             "Password Salah", Toast.LENGTH_LONG).show();
-                }
-                else {
+                } else {
                     Toast.makeText(getApplicationContext(),
                             "Email dan Password salah", Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Method untuk menampilkan menu.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Membuat kondisi jika yang dipilih adalah id mDaftar
+        if (item.getItemId() == R.id.mnDaftar) {
+            //Method untuk memanggil activity "DaftarActivity"
+            Intent i = new Intent(getApplicationContext(), DaftarActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
 
